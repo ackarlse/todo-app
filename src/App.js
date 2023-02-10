dc34380b561b090bc294e4dbdd874c0156ad4a44
ackarlse <ann.c.karlsen@hiof.no>
2023-02-10 11:32:19 +0100

    La til layout comp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Todos from './components/Todos';
+import Form from './components/Form';
+import { useState } from 'react';
+
+const todoStart = [
+  {
+    id:0,
+    title: "Lage middag",
+    content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias"
+  }
+]
 
 function App() {
+  const [todo, setTodo] = useState({title: "", content: ""})
+  const [todos, setTodos] = useState([...todoStart])
+  const [id, setId] = useState(1)
+
+  const addToDo = (event) =>{
+    const inputName = event.target.name
+    const inputValue = event.target.value
+    setTodo((prev) => ({...prev, [inputName]: inputValue}))
+  }
+
+  const handleClick = () => {
+    setId((prev) => prev + 1)
+    setTodos((prev) => [
+      {id, title: todo.title, content: todo.content}, ...prev
+    ])
+
+  }
+
+  const removeToDos = (todoid) =>{
+    setTodos(todos.filter((item) => item.id !== todoid))
+    console.log("Funker")
+    console.log(todos)
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar username="ackarlse"/>
+    <section className='container'>
+      <Form handleClick={handleClick} addToDo={addToDo} todo={todo} />
+    </section>
+    <Todos todoStart={todos} removeToDos={removeToDos}/>
+    </>
   );
 }
 
